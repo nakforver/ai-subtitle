95d72a59dc0a0047ba36cbba96f62a31dc0ee585
nakforver <nakforver@gmail.com>
2026-05-30 15:43:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post("/generate", async (req, res) => {
 
     const model =
       genAI.getGenerativeModel({
-        model: "gemini-1.5-flash"
+        model: "gemini-3.5-flash"
       });
 
     const prompt = `
